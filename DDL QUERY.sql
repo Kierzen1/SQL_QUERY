409da45c8e20cec03b56c9b306959db66417e326
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-09-19 09:55:04 +0800

    Add files via upload

---
+++ DDL QUERY.sql
@@ -0,0 +1,22 @@
+﻿insert into Customer
+values('Leo Bermudez', 'lbermudez@gmail.com','Cebu city');
+insert into Customer
+values('Jennifer Amores', 'j_amores@gmail.com','lapu-lapu city');
+insert into Customer
+values('Jeff Salimbangon', 'js@gmail.com','Mandaue city');
+insert into Cart
+values(1, '9/06/2023')
+insert into Cart
+values(2,'9/7/2023')
+insert into Cart
+values(3,'9/11/2023')
+insert into orders
+values(1,'9/07/2023',1700,'Cebu',2)
+insert into orders
+values(2,'9/07/2023',1100,'Cebu',1)
+insert into orders
+values(3,'9/07/2023',1650,'Cebu',3)
+insert into Book
+values(12345,'DBMS','John Doe','Education',850.00,2,1)
+insert into Book
+values(12346,'Data Structures II','Elmer Ty','Education',550.00,1,3)
