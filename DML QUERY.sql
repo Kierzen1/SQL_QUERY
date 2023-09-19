77e7b5f2f05f6e7ae20af0550239605c8fd68980
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-09-19 09:55:28 +0800

    Add files via upload

---
+++ DML QUERY.sql
@@ -0,0 +1,15 @@
+﻿
+select 
+c.c_id as [Customer ID],
+c.c_name as [Customer Name],
+c.c_adress as [Customer Address]
+from  Customer c 
+where c.c_adress = 'lapu-lapu city'
+
+
+select c_id, c_name, c_email from Customer
+
+select c.c_id as [Customer Identification] from Customer c
+order by c.c_id desc
+
+
