285b8ff976b523b75d6f71accd4c564e969bed11
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-09-21 10:08:55 +0800

    Add files via upload

---
+++ dbo.Book.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Book] (
+	[b_id] INT NOT NULL,
+    [book_isbn]   INT             NOT NULL,
+    [book_title]  NVARCHAR (50)   NULL,
+    [book_author] NVARCHAR (50)   NULL,
+    [book_genre]  NVARCHAR (50)   NULL,
+    [book_price]  DECIMAL (10, 2) NULL,
+    [o_id]        INT             NULL,
+    [cart_id]     INT             NULL,
+    PRIMARY KEY CLUSTERED ([book_isbn] ASC),
+    FOREIGN KEY ([o_id]) REFERENCES [dbo].[Orders] ([o_id])
+);
+
