fda9e6abb9290e73f99154404ec9b81544fa763c
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-09-21 10:10:13 +0800

    Add files via upload

---
+++ dbo.cart.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[cart] (
+    [cart_id]      INT  NOT NULL,
+    [date_created] DATE NULL,
+    PRIMARY KEY CLUSTERED ([cart_id] ASC),
+    FOREIGN KEY ([cart_id]) REFERENCES [dbo].[Customer] ([c_id])
+);
+
