0b6ab1a057adc618de41feba81acfad6138c9a93
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-09-21 10:09:47 +0800

    Add files via upload

---
+++ dbo.Orders.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [o_id]             INT             NOT NULL,
+    [o_date]           DATE            NULL,
+    [total_amt]        DECIMAL (10, 2) NULL,
+    [shipping_address] NVARCHAR (100)  NULL,
+    [c_id]             INT             NULL,
+    PRIMARY KEY CLUSTERED ([o_id] ASC),
+    FOREIGN KEY ([c_id]) REFERENCES [dbo].[Customer] ([c_id])
+);
+
