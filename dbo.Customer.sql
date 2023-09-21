7130d84dc1ab0369d17ebc12c3f7d133266adce1
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-09-21 10:09:20 +0800

    Add files via upload

---
+++ dbo.Customer.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [c_id]     INT            IDENTITY (1, 1) NOT NULL,
+    [c_name]   NVARCHAR (50)  NULL,
+    [c_email]  NVARCHAR (50)  NULL,
+    [c_adress] NVARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([c_id] ASC)
+);
+
