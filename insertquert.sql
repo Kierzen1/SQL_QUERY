531eb321606f9b84ccf4277ec09286e1f95b0b06
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-09-28 10:10:49 +0800

    Add files via upload

---
+++ insertquert.sql
@@ -0,0 +1,5 @@
+﻿insert Into Items(i_id, isbn, title, author, genre, price, booktype, o_id, c_id )
+VALUES
+	(2, 12365, 'DaStruct', 'Tosabel', 'Educational', '1234', 'Audio Books',2, 2),
+	(3, 12445, 'DBMS', 'booc', 'Music', '1234', 'Audio Books',2, 3);
+	
