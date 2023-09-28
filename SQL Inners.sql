dbf772ad374c6b6f36aca5b9d3786537918d5484
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-09-28 10:11:03 +0800

    Add files via upload

---
+++ SQL Inners.sql
@@ -0,0 +1,12 @@
+﻿SELECT
+    Orders.o_id,
+    Customer.c_name,
+    Items.title
+FROM
+    Orders
+ JOIN
+    Customer ON Orders.c_id = Customer.c_id
+INNER JOIN
+    Items ON Orders.o_id = Items.o_id
+WHERE
+    Customer.c_name = 'Leo Bermudez';
