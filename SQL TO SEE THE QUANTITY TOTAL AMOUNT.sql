fed97c400b0d8c4941d51e1a1d1a3ca99e5f48fa
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2023-10-03 09:57:02 +0800

    Add files via upload

---
+++ SQL TO SEE THE QUANTITY TOTAL AMOUNT.sql
@@ -0,0 +1,5 @@
+﻿select o.o_id, c.c_name, i.title,i.price, od.qty *i.price as TotalAmount, od.qty from OrderDetails od
+inner join Orders o on o.o_id = od.o_id
+inner join Items i on i.i_id = od.o_id
+inner join Customer c on c.c_id = o.o_id
+ 
